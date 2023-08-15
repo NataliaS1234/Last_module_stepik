7095ced22d74f1127be1cd932abe849102001e23
Natalia Sayenko <natalia.sayenko@gmail.com>
2023-08-15 16:13:53 +0200

    added conftest.py

---
+++ conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="en", help="Specify the language for the test page")
+    parser.adoption("--browser_name", action="store", default="chrome", help="Specify the browser for the test page")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("--language")
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("prefs", {"intl.accept_languages": language})
+    options_firefox = OptionsFirefox()
+    options_firefox.set_preference("intl.accept_languages", language)
+    browser = None
+    if browser_name == "chrome":
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        browser = webdriver.Firefox(options=options_firefox)
+    yield browser
+    browser.quit()
+
+
+
+
+
