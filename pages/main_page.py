06361fbf997de222ace55cbc252aff3279f58eff
Natalia Sayenko <natalia.sayenko@gmail.com>
2023-08-15 19:11:14 +0200

    added main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,12 @@
-from .base_page import BasePage
+from.base_page import BasePage
+from selenium.webdriver.common.by import By
+#/корень текущего диска
+# ./текущая директория
+# ../родитель текущей директории
 
 
 class MainPage(BasePage): #класс предок указывается в скобках
-    def go_to_login_page(browser):
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
