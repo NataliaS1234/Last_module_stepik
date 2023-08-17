bd4cf69c18a59d3f0b9efff057cd97e32c325d92
Natalia Sayenko <natalia.sayenko@gmail.com>
2023-08-17 14:24:02 +0200

    learn how to switch beteween pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from.base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 #/корень текущего диска
 # ./текущая директория
@@ -12,10 +13,10 @@ class MainPage(BasePage): #класс предок указывается в с�
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
-      assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
 
 
