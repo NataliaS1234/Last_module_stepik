483103b272485ec1b6c3a0db55c25608dd13dfb8
Natalia Sayenko <natalia.sayenko@gmail.com>
2023-08-17 13:48:46 +0200

    adding login_page and fixes in the main_page and test_main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,23 @@
 from.base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+
 #/корень текущего диска
 # ./текущая директория
 # ../родитель текущей директории
+#  хорошей практикой является выносить селектор во внешнюю переменную.
 
 
 class MainPage(BasePage): #класс предок указывается в скобках
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+
+    def should_be_login_link(self):
+      assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def should_be_login_link(self):
+      assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+
 
