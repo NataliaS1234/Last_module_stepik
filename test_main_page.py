158b764ae1dec668d3216b6ecb053b0dda290036
Natalia Sayenko <natalia.sayenko@gmail.com>
2023-08-17 11:13:03 +0200

    refactoring of the file according to PageObject

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,14 @@
 # Здесь и далее мы будем использовать эту команду для запуска.
 # В этой команде мы использовали опцию PyTest --tb=line, которая указывает, что нужно выводить только одну строку из лога каждого упавшего теста.
 # Так вам будет проще разобраться в том, как выглядят сообщения об ошибках.
+#/корень текущего диска
+# ./текущая директория
+# ../родитель текущей директории
 
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 
 def go_to_login_page(browser):
@@ -14,5 +18,6 @@ def go_to_login_page(browser):
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page()
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
